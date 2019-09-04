4c94bbc73f43977b06860efa7d2a6f269b98a0e9
Kolo2142 <colocaceres@gmail.com>
2019-09-04 13:55:00 -0300

    Docker modification and loader added

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,3 @@
-FROM openjdk:8-jdk-alpine
-VOLUME /tmp
-COPY ./target/InternetProtocolNumberTracing-0.0.1-SNAPSHOT.war app.war
-EXPOSE 8080
-ENTRYPOINT ["java","-Djava.security.egd=file:/dev/./urandom","-jar","/app.war"]
+FROM tomcat:9.0-jre8-alpine
+
+HEALTHCHECK --interval=1m --timeout=3s CMD wget --quiet --tries=1 --spider http://localhost:8080/wizard/ || exit 1
--- src/main/resources/static/js/application.js
+++ src/main/resources/static/js/application.js
@@ -2,33 +2,48 @@ var app = angular.module('traceApp', []);
 app.controller('traceController', function($scope, $http, $location) {
 	
 	$scope.traceInternetProtocolNumber = function () {
-		$http.get($location.protocol() + "://" + $location.host()+ ":" + $location.port() + '/process/' + $scope.ipNumber)
-	    .then(function(response) {
-	        $scope.information = response.data;
-	    })
-	    .catch(function (data) {
-	    	alert("The IP number may be wrong, empty or not registered");
-	    });
+		$scope.traceLoading = true;
+		var number = $scope.ipNumber;
+		if (number != undefined && number != "") {
+			$http.get($location.protocol() + "://" + $location.host()+ ":" + $location.port() + '/process/' + $scope.ipNumber)
+		    .then(function(response) {
+		        $scope.information = response.data;
+		        $scope.traceLoading = false;
+		    })
+		    .catch(function (data) {
+		    	alert("The IP number may be wrong or not registered");
+		    	$scope.traceLoading = false;
+		    });
+		} else {
+			alert("The IP number can't be empty");
+			$scope.traceLoading = false;
+		}
 	}
 	
 	$scope.getFurthestRequest = function () {
+		$scope.furthestLoading = true;
 		$http.get($location.protocol() + "://" + $location.host()+ ":" + $location.port() + '/furthest')
 		.then(function(response) {
 	        $scope.furthest = response.data;
+	        $scope.furthestLoading = false;
 	    });
 	}
 	
 	$scope.getNearestRequest = function () {
+		$scope.nearestLoading = true;
 		$http.get($location.protocol() + "://" + $location.host()+ ":" + $location.port() + '/nearest')
 		.then(function(response) {
 	        $scope.nearest = response.data;
+	        $scope.nearestLoading = false;
 	    });
 	}
 	
 	$scope.getAverageRequests = function () {
+		$scope.averageLoading = true;
 		$http.get($location.protocol() + "://" + $location.host()+ ":" + $location.port() + '/average')
 		.then(function(response) {
 	        $scope.average = response.data;
+			$scope.averageLoading = false;
 	    });
 	}
 });
